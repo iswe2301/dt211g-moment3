a8c5ad0b07055983810587663a5c7472016aea00
Isa Westling <isawestling.blogg@hotmail.com>
2024-02-16 10:57:39 +0100

    Lagt till händelselyssnare för autocomplete som vid val av plats anropar funktion för att placera ut markering.

--- src/js/map.js
+++ src/js/map.js
@@ -29,16 +29,50 @@ async function createAutocomplete() {
     try {
         // Inväntar att createMap har slutförts
         await createMap();
-        // Skapar ny instans av autocomplete från google maps javascript places api
-        autocomplete = new google.maps.places.Autocomplete(
+        // Skapar ny instans av autocomplete från google maps javascript places api och lagrar i variabel
+        let autocomplete = new google.maps.places.Autocomplete(
             // Kopplar autocomplete funktionen till search-elementet
             searchInputEl, {
             types: ['establishment', 'geocode'], // Specificerar typer av sökning till både platser och adresser
             fields: ['geometry', 'name'], // Specificerar de fält som ska returneras när en plats väljs till geografisk plats och platsens namn
             language: 'sv' // Sätter språket till svenska
         });
-        // Fångar upp eventuella felmeddelanden
+
+        // Lägger till lyssnare för när användaren väljer en plats från listan
+        autocomplete.addListener('place_changed', async function () {
+            // Hämtar den aktuella platsen som är vald i autocomplete-listan
+            const place = autocomplete.getPlace();
+            // Anropar funktion med platsen som argument
+            displayMarker(place);
+        });
     } catch (error) {
         console.error('Felmeddelande:', error);
+    }
+}
+
+// Anropar funktionen
+createAutocomplete();
+
+// Funktion för att placera ut markering på kartan
+function displayMarker(place) {
+    // Kontrollerar om platsen har en geografisk position
+    if (place.geometry) {
+        const location = place.geometry.location; // Hämtar platsens geografiska position
+        const name = place.name; // Hämtar platsens namn
+
+        // Kontrollerar om det redan finns en markering på kartan
+        if (marker) {
+            marker.setMap(null); // Tar bort markering
+        }
+
+        // Skapar en ny markering på kartan
+        marker = new google.maps.Marker({
+            position: location, // Sätter markeringens position till platsens geografiska position
+            map: map, // Visar markeringen på den befintliga kartan
+            title: name // Sätter titel på markeringen till platsens namn
+        });
+
+        map.setCenter(location); // Centrerar kartan på den valda platsen
+        map.setZoom(18); // Zoomar in kartan till nivå 18
     }
 }
